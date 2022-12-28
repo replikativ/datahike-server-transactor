6f754de22baf717dd443d123422e94f88f7cb13e
Christian Weilbach <ch_weil@topiq.es>
2022-12-28 10:37:00 +0100

    Impplement REST call directly for now since client is not yet done.

--- src/datahike_server_transactor/core.clj
+++ src/datahike_server_transactor/core.clj
@@ -1,28 +1,52 @@
 (ns datahike-server-transactor.core
-  (:require [datahike-client.api :as dc]
+  (:require [clojure.edn :as edn]
             [datahike.transactor :refer [PTransactor create-transactor]]
+            [clj-http.client :as client]
             [taoensso.timbre :as log]
             [clojure.core.async :refer [promise-chan put!]]))
 
+(def edn-fmt "application/edn")
 
-(defrecord DatahikeServerTransactor [connection client client-config]
+(defn api-request
+  ([method url]
+   (api-request method url nil))
+  ([method url data]
+   (api-request method url data nil))
+  ([method url data opts]
+   (let [encode str]
+     (-> (client/request (merge {:url url
+                                 :method method
+                                 :throw-exceptions? false
+                                 :content-type edn-fmt
+                                 :accept edn-fmt}
+                                (when (or (= method :post) data)
+                                  {:body (encode data)})
+                                opts))
+        :body
+        edn/read-string ))))
+
+(defrecord DatahikeServerTransactor [client-config]
   PTransactor
   (send-transaction! [_ tx-data _]
     (let [p (promise-chan)]
-      (log/debug "Sending transaction to datahike-server" client-config tx-data)
-      (put! p (dc/transact connection {:db-name (:db-name client-config)
-                                       :tx-data tx-data}))
+      (log/debug "Sending transaction to datahike-server" client-config (count tx-data))
+      (log/trace "Transacting data " tx-data)
+      (put! p
+            (api-request "post"
+                         (str (:endpoint client-config) "transact")
+                         {:tx-data tx-data}
+                         {:headers
+                          (merge
+                           {:db-name (:db-name client-config)}
+                           (when (:token client-config)
+                             {:authorization (:token client-config)}))}))
       p))
-  (shutdown [_]
-      ;; TODO shutdown client here
-    )
+  (shutdown [_])
   (streaming? [_] false))
 
 (defmethod create-transactor :datahike-server
   [config _ _]
   (log/debug "Creating datahike-server transactor for " config)
-  (let [client-config (:client-config config)
-        client (dc/client client-config)
-        connection (dc/connect client (:db-name client-config))]
-    (->DatahikeServerTransactor connection client client-config)))
+  (let [client-config (:client-config config)]
+    (->DatahikeServerTransactor client-config)))
 
