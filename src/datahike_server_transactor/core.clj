b03ec9af32406dfd1f776e6eda73ae5199bfd05f
Christian Weilbach <ch_weil@topiq.es>
2023-03-29 01:53:35 -0700

    Use store-identity and pass through errors.

--- src/datahike_server_transactor/core.clj
+++ src/datahike_server_transactor/core.clj
@@ -1,5 +1,6 @@
 (ns datahike-server-transactor.core
   (:require [datahike.writer :refer [PWriter create-writer create-database delete-database]]
+            [datahike.store :refer [store-identity]]
             [clj-http.lite.client :as client]
             [taoensso.timbre :as log]
             [clojure.core.async :refer [promise-chan put!]]
@@ -40,28 +41,32 @@
       (log/debug "Sending operation to datahike-server:" op)
       (log/trace "Arguments:" arg-map)
       (put! p
-            (api-request "post"
-                         (str (:endpoint client-config) "/" op)
-                         (first args)
-                         {:headers
-                          (merge
-                           {"db-name" (:db-name client-config)}
-                           (when (:token client-config)
-                             {"authorization" (:token client-config)}))}))
+            (try
+              (api-request "post"
+                           (str (:endpoint client-config) "/" op)
+                           (first args)
+                           {:headers
+                            (merge
+                             {"store-identity" (:store-identity client-config)}
+                             (when (:token client-config)
+                               {"authorization" (:token client-config)}))})
+              (catch Exception e
+                e)))
       p))
   (-shutdown [_])
   (-streaming? [_] false))
 
 (defmethod create-writer :datahike-server
-  [config _]
+  [config connection]
   (log/debug "Creating datahike-server writer for " config)
-  (let [client-config (:client-config config)]
+  (let [client-config (update (:client-config config) :store-identity
+                              #(or % (pr-str (store-identity (:store (:config @(:wrapped-atom connection)))))))]
     (->DatahikeServerWriter client-config)))
 
 (defmethod create-database :datahike-server
-  [& args]
+  [& _args]
   (throw (ex-info "Not supported yet." {:type :not-supported-yet})))
 
 (defmethod delete-database :datahike-server
-  [& args]
+  [& _args]
   (throw (ex-info "Not supported yet." {:type :not-supported-yet})))
